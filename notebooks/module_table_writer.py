58003c0d7ee9177e51084e02c4fa1013e9dfeed2
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2024-01-15 19:02:34 +0100

    new utilitary function

--- notebooks/module_table_writer.py
+++ notebooks/module_table_writer.py
@@ -69,5 +69,38 @@ def table_to_file(df_data, tables_path, tables_file_name):
             out.write("\\rowcolors{1}{}{lightgray}\n")
             out.write(df_data)
    
-#----------------------------------------
+#-----------------------------------------------------------------
+def generate_options_dictionary(filtered_df):
+    """LOAD Alternatives for the Question into a dictionary"""
 
+    # Split the string into key-value pairs
+    answer_options = filtered_df['Alternatives']
+
+    # Split the string into key-value pairs and flatten the list of lists 
+    # This is necessary because Pandas DataFrame is horrible, it always returns a series instead of a primitive object like a string or something.
+    # Therefore, one needs these ugly two for-loops just to get f*ying single list of strings... f*ck...)
+    all_options_list = [option for sublist in answer_options.str.split(';') for option in sublist]
+
+    # Assuming all_options_list is defined earlier in your code
+
+    if all_options_list is not None and all_options_list:
+        # Create a dictionary to store the key-value pairs
+        options_dict = {}
+
+        # Iterate over the pairs and populate the dictionary
+        for pair in all_options_list:
+            # Check if the pair is not None
+            if pair is not None:
+                # Split the pair by ":" and handle potential errors
+                try:
+                    key, value = pair.split(":")
+                    options_dict[key] = value
+                except ValueError:
+                    print(f"Error processing pair: {pair}")
+
+        # Print the resulting dictionary
+        print(options_dict)
+    else:
+        print("all_options_list is None or empty.")
+
+    return options_dict
