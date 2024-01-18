31189f7681034359b2a3921668dfefc1564c4626
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2024-01-18 21:40:11 +0100

    clean up

--- notebooks/module_table_writer.py
+++ notebooks/module_table_writer.py
@@ -7,11 +7,6 @@ import module_table_writer as table_writer
 def write_latex_table(data_df, show_index, filter_value, 
                       question_id, question_title,column_format):
     """ Generate Latex table and save to file"""
-    
-    #if styles is not None:
-    #    count_df = data_df.style.set_table_styles(styles)
-    #else:
-    #    count_df=data_df
 
     table_caption=f'{filter_value} answers for {question_id}-{question_title}'
     label_name = f'{question_id}-{filter_value}_table'
@@ -32,7 +27,6 @@ def table_to_file(df_data, tables_path, tables_file_name):
         os.makedirs(tables_path)
 
     file_name = f'{tables_path}{tables_file_name}_tables.tex'
-    #print("file_name" + file_name)
     
     if(not os.path.isfile(file_name)):
         with open(file_name, 'w') as out:
